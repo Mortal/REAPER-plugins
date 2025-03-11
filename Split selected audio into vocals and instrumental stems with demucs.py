c5bf1749feeaea0727ef72ee227ab7a9de9c9379
Mathias Rav <m@git.strova.dk>
2025-03-11 16:00:17 +0000

    WIP

--- Split selected audio into vocals and instrumental stems with demucs.py
+++ Split selected audio into vocals and instrumental stems with demucs.py
@@ -4,6 +4,7 @@ from typing import Literal
 
 from reaper_python import *
 from reaper_loop import reaper_loop_run
+import autil
 import rutil
 
 
@@ -12,40 +13,10 @@ async def amain() -> None:
     two_stems: Literal["bass", "drums", "vocals"] | None = None  # "vocals"
 
     item = rutil.script_get_single_selected_media_item()
-    track = RPR_GetMediaItem_Track(item)
-    take = RPR_GetActiveTake(item)
-    src = RPR_GetMediaItemTake_Source(take)
-    src, path, _ = RPR_GetMediaSourceFileName(src, "", rutil.MAX_STRBUF)
-
-    assert os.path.exists(path)
-    dirname, filename = os.path.split(path)
-    basename = os.path.splitext(path)[0]
-
-    sourcelength, src, quarternotes = RPR_GetMediaSourceLength(src, False)
-    assert not quarternotes
-    itempos = RPR_GetMediaItemInfo_Value(item, "D_POSITION")
-    length = RPR_GetMediaItemInfo_Value(item, "D_LENGTH")
-    startoffs = RPR_GetMediaItemTakeInfo_Value(take, "D_STARTOFFS")
-    playrate = RPR_GetMediaItemTakeInfo_Value(take, "D_PLAYRATE")
-    timestart, timeend = itempos, itempos + length
-    timestart, timeend = rutil.range_intersect(rutil.get_time_selection(), (timestart, timeend))
-    assert timestart < timeend
-    sourcestart = startoffs + (timestart - itempos) * playrate
-    sourceend = startoffs + (timeend - itempos) * playrate
-    sourcefrac = (sourceend - sourcestart) / sourcelength
-    if sourcefrac < 0.5:
-        cutbase = f"{basename}_{sourcestart*1000:.0f}_{sourceend*1000:.0f}"
-        cutfile = os.path.join(dirname, f"{cutbase}.flac")
-        if not os.path.exists(cutfile):
-            proc = await asyncio.subprocess.create_subprocess_exec(
-                "gnome-terminal", "--wait", "--",
-                'ffmpeg', '-i', path, '-ss', str(sourcestart), '-to', str(sourceend), '-y', cutfile
-            )
-            await proc.wait()
-        path = cutfile
-        basename = cutbase
-        sourceend -= sourcestart
-        sourcestart = 0
+    track = item.track
+    source_slice = autil.script_get_selected_audio_source(item)
+    if source_slice.slice_fraction < 0.5:
+        source_slice = await autil.cut_source_slice_into_new_file(source_slice)
 
     if two_stems is not None:
         two_stems_arg = ["--two-stems", two_stems]
@@ -54,34 +25,35 @@ async def amain() -> None:
         two_stems_arg = []
         outputs = ["bass", "drums", "vocals", "other"]
 
+    dirname = os.path.dirname(source_slice.path)
+    basename = os.path.basename(source_slice.path)
     filename_fmt = f'{basename}_{{stem}}_split_by_demucs.wav'
     filenames = [filename_fmt.format(stem=s) for s in outputs]
     paths = [os.path.join(dirname, modelname, f) for f in filenames]
     if not all(os.path.exists(p) for p in paths):
         proc = await asyncio.subprocess.create_subprocess_exec(
             "gnome-terminal", "--geometry=122x10", "--wait", "--",
-            "demucs", *two_stems_arg, "-n", modelname, "--float32", "-o", dirname, "--filename", filename_fmt, path
+            "demucs", *two_stems_arg, "-n", modelname, "--float32", "-o", dirname, "--filename", filename_fmt, source_slice.path
         )
         exitcode = await proc.wait()
         if exitcode:
             raise Exception(f"gnome-terminal/demucs failed with exit code {exitcode}")
 
     with rutil.undoblock("Split stems"):
-        RPR_SetMediaItemSelected(item, False)
+        item.selected = False
         rutil.clear_selection()
         items = []
         for stem_path in paths:
             RPR_InsertMedia(stem_path, 1)
             item2 = rutil.script_get_single_selected_media_item()
-            take2 = RPR_GetActiveTake(item2)
-            RPR_SetMediaItemInfo_Value(item2, "D_POSITION", timestart)
-            RPR_SetMediaItemInfo_Value(item2, "D_LENGTH", timeend - timestart)
-            RPR_SetMediaItemTakeInfo_Value(take2, "D_PLAYRATE", playrate)
-            RPR_SetMediaItemTakeInfo_Value(take2, "D_STARTOFFS", sourcestart)
-            RPR_SetMediaItemSelected(item2, False)
+            take2 = item2.active_take
+            item2.time_range = source_slice.item_time_range
+            take2.playrate = source_slice.playrate
+            take2.startoffs = source_slice.startoffs
+            item2.selected = False
             items.append(item2)
         rutil.set_selection(items)
-        RPR_SetMediaTrackInfo_Value(track, "B_MUTE", 1.0)
+        track.muted = True
 
 
 def main() -> None:
--- rutil.py
+++ rutil.py
@@ -1,6 +1,6 @@
 import contextlib
 from dataclasses import dataclass
-from typing import Iterator
+from typing import Any, Iterator
 
 from reaper_python import *
 
@@ -20,27 +20,35 @@ def undoblock(name: str) -> Iterator[UndoBlock]:
         RPR_Undo_EndBlock2(None, block.name, 0)
 
 
-def clear_selection():
-    items = [RPR_GetSelectedMediaItem(None, i) for i in range(RPR_CountSelectedMediaItems(None))]
-    for item in items:
-        RPR_SetMediaItemSelected(item, False)
-    return items
-
-
-def set_selection(items):
-    clear_selection()
-    select_all(items)
-
-
-def select_all(items):
-    for item in items:
-        RPR_SetMediaItemSelected(item, True)
-
-
 @dataclass
 class TimeRange:
     start: float
     end: float
+
+    @property
+    def valid_closed(self) -> bool:
+        return self.start <= self.end
+
+    @property
+    def valid_open(self) -> bool:
+        return self.start < self.end
+
+    def __add__(self, rhs: float) -> TimeRange:
+        return TimeRange(self.start + rhs, self.end + rhs)
+
+    def __sub__(self, rhs: float) -> TimeRange:
+        return TimeRange(self.start - rhs, self.end - rhs)
+
+    def __mul__(self, rhs: float) -> TimeRange:
+        return TimeRange(self.start * rhs, self.end * rhs)
+
+    def __div__(self, rhs: float) -> TimeRange:
+        return TimeRange(self.start / rhs, self.end / rhs)
+
+    @property
+    def length(self) -> float:
+        assert self.valid_closed
+        return self.end - self.start
 
 
 def get_time_selection() -> TimeRange | None:
@@ -52,23 +60,156 @@ def get_time_selection() -> TimeRange | None:
     return TimeRange(startOut, endOut)
 
 
-def script_get_single_selected_media_item() -> "MediaItem":
+MAX_STRBUF = 4 * 1024 * 1024
+
+
+def range_intersect(a: TimeRange | None, b: TimeRange) -> TimeRange:
+    if a is None:
+        return b
+    p, q = a.start, a.end
+    r, s = b.start, b.end
+    x = max(p, r)
+    y = min(q, s)
+    return TimeRange(x, y)
+
+
+@dataclass
+class RTrack:
+    track: Any
+
+    @property
+    def muted(self) -> bool:
+        return bool(RPR_GetMediaTrackInfo_Value(self.track, "B_MUTE"))
+
+    @muted.setter
+    def muted(self, v: bool) -> None:
+        RPR_SetMediaTrackInfo_Value(self.track, "B_MUTE", 1.0 if v else 0.0)
+
+
+
+@dataclass
+class RMediaSource:
+    src: Any
+
+    @property
+    def path(self) -> str:
+        _src, path, _ = RPR_GetMediaSourceFileName(self.src, "", MAX_STRBUF)
+        return path
+
+    @property
+    def length_seconds_or_quarters(self) -> tuple[float, bool]:
+        sourcelength, src, quarternotes = RPR_GetMediaSourceLength(self.src, False)
+        return sourcelength, quarternotes
+
+    @property
+    def length_seconds(self) -> float:
+        sourcelength, quarternotes = self.length_seconds_or_quarters
+        assert not quarternotes
+        return sourcelength
+
+    @property
+    def maybe_length_seconds(self) -> float | None:
+        sourcelength, quarternotes = self.length_seconds_or_quarters
+        return None if quarternotes else sourcelength
+
+
+@dataclass
+class RMediaItemTake:
+    take: Any
+
+    @property
+    def source(self) -> RMediaSource:
+        return RMediaSource(RPR_GetMediaItemTake_Source(self.take))
+
+    @property
+    def startoffs(self) -> float:
+        return RPR_GetMediaItemTakeInfo_Value(self.take, "D_STARTOFFS")
+
+    @startoffs.setter
+    def startoffs(self, v: float) -> None:
+        RPR_SetMediaItemTakeInfo_Value(self.take, "D_STARTOFFS", v)
+
+    @property
+    def playrate(self) -> float:
+        return RPR_GetMediaItemTakeInfo_Value(self.take, "D_PLAYRATE")
+
+    @playrate.setter
+    def playrate(self, v: float) -> None:
+        RPR_SetMediaItemTakeInfo_Value(self.take, "D_PLAYRATE", v)
+
+
+@dataclass
+class RMediaItem:
+    item: Any
+
+    @property
+    def active_take(self) -> RMediaItemTake:
+        return RMediaItemTake(RPR_GetActiveTake(self.item))
+
+    @property
+    def track(self) -> RTrack:
+        return RTrack(RPR_GetMediaItem_Track(self.item))
+
+    @property
+    def position(self) -> float:
+        return RPR_GetMediaItemInfo_Value(self.item, "D_POSITION")
+
+    @position.setter
+    def position(self, p: float) -> None:
+        RPR_SetMediaItemInfo_Value(self.item, "D_POSITION", p)
+
+    @property
+    def length(self) -> float:
+        return RPR_GetMediaItemInfo_Value(self.item, "D_LENGTH")
+
+    @length.setter
+    def length(self, p: float) -> None:
+        RPR_SetMediaItemInfo_Value(self.item, "D_LENGTH", p)
+
+    @property
+    def time_range(self) -> TimeRange:
+        p = self.position
+        return TimeRange(p, p + self.length)
+
+    @time_range.setter
+    def time_range(self, span: TimeRange) -> None:
+        self.position = span.start
+        self.length = span.end - span.start
+
+    @property
+    def selected(self) -> bool:
+        return RPR_IsMediaItemSelected(self.item)
+
+    @selected.setter
+    def selected(self, v: bool) -> None:
+        RPR_SetMediaItemSelected(self.item, v)
+
+
+def script_get_single_selected_media_item() -> RMediaItem:
     count = RPR_CountSelectedMediaItems(None)
     if not count:
         raise SystemExit("Please select a media item")
     if count > 1:
         raise SystemExit("Please select a single media item")
-    return RPR_GetSelectedMediaItem(None, 0)
+    return RMediaItem(RPR_GetSelectedMediaItem(None, 0))
 
 
-MAX_STRBUF = 4 * 1024 * 1024
+def get_selection():
+    return [RMediaItem(RPR_GetSelectedMediaItem(None, i)) for i in range(RPR_CountSelectedMediaItems(None))]
 
 
-def range_intersect(
-    a: TimeRange | None, b: tuple[float, float]
-) -> tuple[float, float]:
-    if a is None:
-        return b
-    p, q = a.start, a.end
-    r, s = b
-    return max(p, r), min(q, s)
+def clear_selection():
+    items = get_selection()
+    for item in items:
+        item.selected = False
+    return items
+
+
+def set_selection(items):
+    clear_selection()
+    select_all(items)
+
+
+def select_all(items):
+    for item in items:
+        item.selected = True
